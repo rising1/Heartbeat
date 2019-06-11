74c4041dbfe85373d49091a999a0b860b68cbdef
rising1 <phfrost@btconnect.com>
2019-06-11 18:00:09 +0100

    Watch the file path

--- HawkNet.py
+++ HawkNet.py
@@ -26,7 +26,7 @@ weight_decay = 0.0001 # used in HeartbeatClean
 dropout_factor = 0.1 # used in Unit
 snapshot_point = 20
 faff = 'false'
-dataPathRoot = 'C:/Users/phfro/Documents/python/data/BirdiesData/' # used in DataLoaderHeartbeat
+dataPathRoot = 'C:/Users/peter.frost/Documents/python/data/BirdiesData/' # used in DataLoaderHeartbeat
 num_epochs = 1 # used in HeartbeatClean
 batch_sizes = 4 # used in HeartbeatClean
 
@@ -40,7 +40,7 @@ optimizer = Adam(model.parameters(), lr=learning_rate,
 loss_fn = nn.CrossEntropyLoss()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-train_loader_class = HawkDataLoader.HawkLoader( \
+train_loader_class = HawkDataLoader.HawkLoader(
                 dataPathRoot, batch_sizes)
 
 train_loader = train_loader_class.dataloaders["train"]
