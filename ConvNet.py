d046899854c6f7d3a1c80e78154176ec88323a84
rising1 <phfrost@btconnect.com>
2019-06-13 18:15:03 +0100

    Watch the file path

--- ConvNet.py
+++ ConvNet.py
@@ -26,22 +26,58 @@ class SimpleNet(nn.Module):
         self.pool1 = nn.MaxPool2d(kernel_size=2)
                                   #  self.pooling_factor)
 
-        self.unit4 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size, out_channels=self.pic_size * 2)
-        self.unit5 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 2, out_channels=self.pic_size * 2)
-        self.unit6 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 2, out_channels=self.pic_size * 2)
-        self.unit7 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 2, out_channels=self.pic_size * 2)
-        self.pool2 = nn.MaxPool2d(kernel_size=self.pooling_factor)
+        self.unit4 = UnitNet.Unit(self.UnitArgs, in_channels=120,
+                                  #  self.pic_size,
+                                  out_channels=240)
+                                  #  self.pic_size * 2)
+        self.unit5 = UnitNet.Unit(self.UnitArgs, in_channels=240,
+                                  #  self.pic_size * 2,
+                                  out_channels=240)
+                                  #  self.pic_size * 2)
+        self.unit6 = UnitNet.Unit(self.UnitArgs, in_channels=240,
+                                  #  self.pic_size * 2,
+                                  out_channels=240)
+                                  #  self.pic_size * 2)
+        self.unit7 = UnitNet.Unit(self.UnitArgs, in_channels=240,
+                                  #  self.pic_size * 2,
+                                  out_channels=240)
+                                  #  self.pic_size * 2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+                                  #  self.pooling_factor)
 
-        self.unit8 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 2, out_channels=self.pic_size * 4)
-        self.unit9 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 4, out_channels=self.pic_size * 4)
-        self.unit10 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 4, out_channels=self.pic_size * 4)
-        self.unit11 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 4, out_channels=self.pic_size * 4)
-        self.pool3 = nn.MaxPool2d(kernel_size=self.pooling_factor)
+        self.unit8 = UnitNet.Unit(self.UnitArgs, in_channels=240,
+                                  #  self.pic_size * 2,
+                                  out_channels=480)
+                                  #  self.pic_size * 4)
+        self.unit9 = UnitNet.Unit(self.UnitArgs, in_channels=480,
+                                  #  self.pic_size * 4,
+                                  out_channels=480)
+                                  #  self.pic_size * 4)
+        self.unit10 = UnitNet.Unit(self.UnitArgs, in_channels=480,
+                                   #  self.pic_size * 4,
+                                   out_channels=480)
+                                  #  self.pic_size * 4)
+        self.unit11 = UnitNet.Unit(self.UnitArgs, in_channels=480,
+                                   #  self.pic_size * 4,
+                                   out_channels=480)
+                                   #  self.pic_size * 4)
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+                                   #  self.pooling_factor)
 
-        self.unit12 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 4, out_channels=self.pic_size * 4)
-        self.unit13 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 4, out_channels=self.pic_size * 4)
-        self.unit14 = UnitNet.Unit(self.UnitArgs, in_channels=self.pic_size * 4, out_channels=self.pic_size * 4)
-        self.avgpool = nn.AvgPool2d(kernel_size=(self.pooling_factor * 2) + 1)
+        self.unit12 = UnitNet.Unit(self.UnitArgs, in_channels=480,
+                                   #  self.pic_size * 4,
+                                   out_channels=480)
+                                   #  self.pic_size * 4)
+        self.unit13 = UnitNet.Unit(self.UnitArgs, in_channels=480,
+                                   #  self.pic_size * 4,
+                                   out_channels=480)
+                                   #  self.pic_size * 4)
+        self.unit14 = UnitNet.Unit(self.UnitArgs, in_channels=480,
+                                   #  self.pic_size * 4,
+                                   out_channels=480)
+                                   #  self.pic_size * 4)
+        self.avgpool = nn.AvgPool2d(kernel_size=4)
+                                   #  (self.pooling_factor * 2) + 1)
 
         self.net = nn.Sequential(self.unit1, self.unit2, self.unit3, \
                                  self.pool1, self.unit4, self.unit5, self.unit6, \
