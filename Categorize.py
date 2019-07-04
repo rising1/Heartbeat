3e4ddd38be1eaa212db56b4bd2ca5d314fe32b6f
rising1 <phfrost@btconnect.com>
2019-07-04 20:22:37 +0100

    Working model

--- Categorize.py
+++ Categorize.py
@@ -2,13 +2,17 @@ import os
 from string import ascii_lowercase
 
 class Categorize:
-    def __init__(self, rootDir,targetDir, catType):
+    def __init__(self, rootDir,targetDir):
         self.rootDir = rootDir
         self.targetDir = targetDir
         self.catType = catType
         for dirPath, dirNames, fileNames in os.walk(self.rootDir):
             print(dirNames)
             for letter in ascii_lowercase:
+                if not ( os.path.isdir(self.targetDir + '/' + letter)):
+                    print(self.targetDir + '/' + letter)
+                    #  os.mkdir(os.path.isdir(self.targetDir + '/' + letter))
+
 
 if __name__ == "__main__":
-        Categorize('root directory','target directory','alphabetic')
+    Categorize('root directory','target directory','alphabetic')
