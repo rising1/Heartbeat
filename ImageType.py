e309d8bb8ee878b734730cf61e2cdb2f630f970a
rising1 <phfrost@ymail.com>
2019-08-03 09:57:13 +0100

    Fine tuning the learning messages

--- ImageType.py
+++ ImageType.py
@@ -12,55 +12,65 @@ from PIL import Image
 import json
 
 
-model = squeezenet1_1(pretrained=True)
-model.eval()
 
 
-def predict_image(image_path):
-    print("Prediction in progress")
-    image = Image.open(image_path)
+class ImageType:
 
-    # Define transformations for the image, should (note that imagenet models are trained with image size 224)
-    transformation = transforms.Compose([
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    model = squeezenet1_1(pretrained=True)
+    model.eval()
+    x = []
 
-    # Preprocess the image
-    image_tensor = transformation(image).float()
+    def __init__(self,image_path):
+        self.image_path = image_path
+        index_file = "class_map.txt"
 
-    # Add an extra batch dimension since pytorch treats all images as batches
-    image_tensor = image_tensor.unsqueeze_(0)
 
-    if torch.cuda.is_available():
-        image_tensor.cuda()
 
-    # Turn the input into a Variable
-    input = Variable(image_tensor)
+        indexpath = os.path.join(os.getcwd(), index_file)
+        # Donwload class index if it doesn't exist
+        if not os.path.exists(indexpath):
+            print("cant find image indeces")
+        with open(indexpath, "r") as f:
+            self.class_map = f.readlines()
+        print("type of self.class_map", type(self.class_map))
 
-    # Predict the class of the image
-    output = model(input)
+        # run prediction function annd obtain predicted class index
+        index = self.predict_image()
+        prediction = self.class_map[index]
+        print("Predicted Class ", prediction)
 
-    index = output.data.numpy().argmax()
+    def predict_image(self):
 
-    return index
+        print("Prediction in progress")
+        image = Image.open(self.image_path)
+
+        # Define transformations for the image, should (note that imagenet models are trained with image size 224)
+        transformation = transforms.Compose([
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        # Preprocess the image
+        image_tensor = transformation(image).float()
+
+        # Add an extra batch dimension since pytorch treats all images as batches
+        image_tensor = image_tensor.unsqueeze_(0)
+
+        if torch.cuda.is_available():
+            image_tensor.cuda()
+
+        # Turn the input into a Variable
+        input = Variable(image_tensor)
+
+        # Predict the class of the image
+        output = self.model(input)
+
+        index = output.data.numpy().argmax()
+
+        return index
 
 
 if __name__ == "__main__":
+    ImageType("H:/birdiesdata2/Accentor/Alpine_1. 550px-prunella_collaris_collaris%2c_picos_de_europa.jpg")
 
-    index_file = "class_map.txt"
-
-    indexpath = os.path.join(os.getcwd(), index_file)
-    # Donwload class index if it doesn't exist
-    if not os.path.exists(indexpath):
-        print("cant find image indeces")
-
-    class_map = json.load(open(indexpath))
-
-    # run prediction function annd obtain prediccted class index
-    index = predict_image(imagepath)
-
-    prediction = class_map[str(index)][1]
-
-    print("Predicted Class ", prediction)
