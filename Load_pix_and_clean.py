ea03aa5dfc4dc8a008904965ee5fdd7282da058c
rising1 <phfrost@ymail.com>
2019-08-16 06:41:34 +0100

    Save scan results

--- Load_pix_and_clean.py
+++ Load_pix_and_clean.py
@@ -76,7 +76,7 @@ class Load_pix():
             response.download(arguments)
         except:
             pass
-        file_path = os.path.join(loc_data,query)
+        file_path = os.path.join(loc_data, query)
         if os.path.isfile(file_path):
             try:
                 result = self.image_type.predict_image(file_path)
