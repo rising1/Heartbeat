26d177ca4a8cc3cedf4826f67d440bacc3ab6606
rising1 <phfrost@btconnect.com>
2019-08-25 08:50:45 +0100

    interim saves

--- Hawknet_Depld.py
+++ Hawknet_Depld.py
@@ -18,15 +18,6 @@ class test_an_image():
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
         plt.show()
 
-    def transfer_to_gpu(self,image_dataset):
-        global device
-        device = torch.device("cuda: 0" if torch.cuda.is_available() else "cpu")
-        print("device=", device)
-        if torch.cuda.is_available():
-            torch.cuda.empty_cache()
-            image_dataset[0][0].to(device)
-            print("image transferred to GPU")
-        return image_dataset[0][0]
 
     def data_transformation(self,dataPathRoot):
         if not (os.path.exists(dataPathRoot)):
@@ -40,9 +31,8 @@ class test_an_image():
 
         #image_dataset = datasets.ImageFolder(os.path.join(dataPathRoot, 'photo.jpg'), data_transform)
         image_dataset = datasets.ImageFolder(os.path.join(dataPathRoot), data_transform)
-        self.imshow(torchvision.utils.make_grid(image_dataset[0][0]))
+        self.imshow(torchvision.utils.make_grid(image_dataset[0]))
         #  push the data to the GPU
-        #  image_dataset = self.transfer_to_gpu(image_dataset)
         image_dataset = image_dataset[0][0]
         return image_dataset
 
