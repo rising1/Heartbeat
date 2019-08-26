b8dfbea8d0ae14c3bf3e9c68432d15d760dc030a
rising1 <phfrost@btconnect.com>
2019-08-26 16:16:34 +0100

    Working model

--- HawkNet.py
+++ HawkNet.py
@@ -344,35 +344,7 @@ def imshow(inp, title=None):
     plt.pause(0.1)  # pause a bit so that plots are updated
 
 
-def show_images(images, cols=1, titles=None):
-    """Display a list of images in a single figure with matplotlib.
 
-    Parameters
-    ---------
-    images: List of np.arrays compatible with plt.imshow.
-
-    cols (Default = 1): Number of columns in figure (number of rows is
-                        set to np.ceil(n_images/float(cols))).
-
-    titles: List of titles corresponding to each image. Must have
-            the same length as titles.
-    """
-    assert ((titles is None) or (len(images) == len(titles)))
-    n_images = len(images)
-    if titles is None: titles = ['Image (%d)' % i for i in range(1, n_images + 1)]
-    fig = plt.figure()
-    for n, (image, title) in enumerate(zip(images, titles)):
-        a = fig.add_subplot(cols, np.ceil(n_images / float(cols)), n + 1)
-        a.set_title(title + " - " + test_single(image))
-        image = image  / 2 + 0.5  # unnormalize
-        npimg = image.numpy()
-        image = np.transpose(npimg, (1, 2, 0))
-        if image.ndim == 2:
-            plt.gray()
-        plt.imshow(image)
-
-    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
-    plt.show()
 
 # train(num_epochs)
 if __name__ == "__main__":
