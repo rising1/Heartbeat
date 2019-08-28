728a9473afe5302b1185e8591b32de48cec663d1
rising1 <phfrost@btconnect.com>
2019-08-28 18:08:06 +0100

    interim saves

--- HawkNet.py
+++ HawkNet.py
@@ -319,7 +319,12 @@ def test_single(images):
         image = image[0].to(device)
         print(type(image))
         img = image.cpu()
-        image_list.append(img)
+        inp = img.numpy().transpose((1, 2, 0))
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        inp = std * inp + mean
+        inp = np.clip(inp, 0, 1)
+        image_list.append(inp)
         outputs = model(image.unsqueeze(0))
         _, prediction = torch.max(outputs.data, 1)
         guess = birds_listing()[int(prediction.cpu().numpy())]
