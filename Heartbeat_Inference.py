fc54ce1efbec7ff37a466616f40ee6b914b11978
rising1 <phfrost@btconnect.com>
2019-09-07 16:44:15 +0100

    Working model

--- Heartbeat_Inference.py
+++ Heartbeat_Inference.py
@@ -19,13 +19,16 @@
 
 import HawkNet
 import Hawknet_Depld
+validate_path = 'C:/Users/phfro/Documents/python/data/Class_validate.txt'
+dataPathRoot = 'C:/Users/phfro/Documents/python/data'
+
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
         return "Hello World!"
 
-HawkNet.build_model()
+HawkNet.build_model(dataPathRoot)
 HawkNet.transfer_to_gpu()
 is_eval = True
 HawkNet.load_latest_saved_model('Birdies_model_0.model_best_acc_4.2667_',is_eval)
@@ -38,5 +41,8 @@ deploy_test = Hawknet_Depld.test_images(
         'C:/Users/phfro/Documents/python/data/eval/' )
 #  HawkNet.show_images(deploy_test.test_image)
 #  HawkNet.imshow(deploy_test.test_images) --> 28.08.19
-HawkNet.test_single(deploy_test.test_images)
+HawkNet.test_single(deploy_test.test_images, validate_path)
 #  /content/drive/My Drive/Colab Notebooks/saved_models/Birdies_model_0.model_best_acc_4.2667
+
+if __name__ == '__main__':
+        app.run(debug=True)
