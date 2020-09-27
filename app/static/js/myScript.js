cecd261773ce13bb2234631bfb5526542dab1c4c
rising1 <phfrost@ymail.com>
2020-09-27 18:08:21 +0100

    extracted javascript to separate js file

--- app/static/js/myScript.js
+++ app/static/js/myScript.js
@@ -7,17 +7,34 @@
     var dropHandler = function(evt){
         evt.preventDefault();
         var files = evt.originalEvent.dataTransfer.files;
-
+        var imageout = new Image();
 
         var reader  = new FileReader();
 
         // it's onload event and you forgot (parameters)
 
         reader.onload = function(e)  {
-            var image = document.createElement("img");
+            //var image = document.createElement("img");
             // the result image data
-            image.src = e.target.result;
-            document.getElementById("prediction").appendChild(image);
+            //image.src = e.target.result;
+            const width = 200;
+            const height = 200;
+            reader.readAsDataURL(files[0]);
+            reader.onload = event => {
+                const img = new Image();
+                img.src = event.target.result;
+                img.onload = () => {
+                    const elem = document.createElement('canvas');
+                    elem.width = width;
+                    elem.height = height;
+                    const ctx = elem.getContext('2d');
+                    // img.width and img.height will contain the original dimensions
+                    ctx.drawImage(img, 0, 0, width, height);
+                    imageout.src = elem.toDataURL("image/png")
+                },
+            reader.onerror = error => console.log(error);
+            };
+            $("#birdpic").empty().append(imageout);
         }
 
         reader.readAsDataURL(files[0]);
