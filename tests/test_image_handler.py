88e0dae2064be1f35b2c044edd0636efefb4828b
katiefrost <katie_frost95@hotmail.co.uk>
2020-10-06 22:54:13 +0100

    updated test to loop through and predict all bird pictures in resournces/bird_pictures

--- tests/test_image_handler.py
+++ tests/test_image_handler.py
@@ -10,14 +10,20 @@ class TestImageHandler:
     #     model_builder.load_and_populate_model(constants.BIRDIES_MODEL)
 
     def test_givenbirdpic_whenPostRequestToUploaderEndpoint_theReturnsPrediction(self):
-        #given
-        image_filepath = os.path.join(constants.RESOURCES_ROOT, 'Mallard.jpg')
+        # given
+        actual_predictions = []
+        images_dir = os.path.join(constants.RESOURCES_ROOT, 'bird_pictures')
 
-        #when
-        prediction = image_handler.handle(image_filepath)
+        # when
+        for bird_pic in os.listdir(images_dir):
+            prediction = image_handler.handle(os.path.join(images_dir, bird_pic))
+            actual_predictions.append(prediction)
 
-        #then
-        assert "Duck" in prediction
+        # then
+        assert "Duck" in actual_predictions[0]
+        assert "robin" in actual_predictions[1]
+        assert "swan" in actual_predictions[2]
+
 
 
 
